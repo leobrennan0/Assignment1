732601c57576d311dc9ca82a0a63f887b188acb1
Leo Brennan <l.brennan-tohill1@nuigalway.ie>
2022-10-18 17:18:06 +0100

    Filled in Student again

---
+++ src/main/java/Student.java
@@ -0,0 +1,28 @@
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+public class Student {
+    String name;
+    int age;
+    LocalDate  dob;
+    int id;
+    String username;
+    Course course;
+    Module[] modules;
+
+    public Student(String name, int age, LocalDate dob, final int id)
+    {
+        this.name = name;
+        this.dob = dob;
+        this.age = age;
+
+        username = "" + name + String.valueOf(age);
+    }
+
+    //returns username
+    public String getUsername()
+    {
+        return username;
+    }
+
+}
